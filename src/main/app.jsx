dd3482f718bedac84ce41ca8c85b0505a56c0a95
Guilherme <mttsguil@gmail.com>
2022-09-22 09:42:07 -0300

    add: component footer

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,10 +3,15 @@ import React from "react";
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/Sidebar';
+import Footer from '../common/template/Footer';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
+        <div className='content-wrapper'>
+            <h1>Content</h1>
+        </div>
+        <Footer />
     </div>
 )
