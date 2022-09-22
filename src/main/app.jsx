3f592ca5ce09b2a1fd6b6bd3f5641a7c50010a3d
Guilherme <mttsguil@gmail.com>
2022-09-22 09:35:49 -0300

    add: component menuTree, menuItem and Menu

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,8 +1,12 @@
 import '../common/template/dependecies'
 import React from "react";
 
+import Header from '../common/template/header';
+import Sidebar from '../common/template/Sidebar';
+
 export default props => (
     <div className="wrapper">
-        <h1>App</h1>
+        <Header />
+        <Sidebar />
     </div>
 )
