bb34f485326b292c9ebee3e83ba40cb6138b9aa6
Guilherme <mttsguil@gmail.com>
2022-09-23 12:08:07 -0300

    add: input component for CreditList

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { reduxForm, Field} from 'redux-form'
-import labelInput from '../common/form/labelInput'
+import LabelInput from '../common/form/labelInput'
 
 import {init} from './billingCycleActions'
 import CreditList from './creditList'
@@ -14,15 +14,15 @@ class billingCycleForm extends Component {
         return(
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelInput}
+                    <Field name='name' component={LabelInput}
                     label='Nome' cols='12 4' readOnly={readOnly} placeholder='Informe o nome' />
-                    <Field name='month' component={labelInput} readOnly={readOnly} 
+                    <Field name='month' component={LabelInput} readOnly={readOnly} 
                     label='Mês' cols='12 4' type="number" placeholder='Informe o mês' />
-                    <Field name='year' component={labelInput}
+                    <Field name='year' component={LabelInput}
                     readOnly={readOnly}
                     label='Ano' cols='12 4' type="number" placeholder='Informe o ano'
                     />
-                    <CreditList cols='12 6' />
+                    <CreditList cols='12 6' readOnly={readOnly}/>
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}> {this.props.submitLabel}</button>
---
+++ src/common/form/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => ( 
+    <input {...props.input} 
+    className='form-control'
+    placeholder={props.placeholder}
+    type={props.type}
+    readOnly={props.readOnly}
+      />
+)
