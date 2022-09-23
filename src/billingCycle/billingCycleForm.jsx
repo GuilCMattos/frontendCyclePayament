504ec2a347d67a2ab527c14b7f9653c8f4f3852e
Guilherme <mttsguil@gmail.com>
2022-09-23 10:22:11 -0300

    add: field with personalize component

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { reduxForm, Field} from 'redux-form'
+import labelInput from '../common/form/labelInput'
 
 class billingCycleForm extends Component { 
     render() { 
@@ -8,9 +9,13 @@ class billingCycleForm extends Component {
         return(
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={labelInput}
+                    label='Nome' cols='12 4' placeholder='Informe o nome' />
+                    <Field name='month' component={labelInput} 
+                    label='Mês' cols='12 4' type="number" placeholder='Informe o mês' />
+                    <Field name='year' component={labelInput}
+                    label='Ano' cols='12 4' type="number" placeholder='Informe o ano'
+                    />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className='btn btn-primary'> Submit</button>
