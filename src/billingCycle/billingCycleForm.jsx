bda292ec9003f574d5e3e717fa5e4d2b240c4a0d
Guilherme <mttsguil@gmail.com>
2022-09-23 12:43:39 -0300

    add: component creditList to itemList

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector} from 'redux-form'
 import LabelInput from '../common/form/labelInput'
 
 import {init} from './billingCycleActions'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class billingCycleForm extends Component { 
     render() { 
@@ -22,7 +22,9 @@ class billingCycleForm extends Component {
                     readOnly={readOnly}
                     label='Ano' cols='12 4' type="number" placeholder='Informe o ano'
                     />
-                    <CreditList cols='12 6' readOnly={readOnly} list={credits}/>
+                    <ItemList cols='12 6' readOnly={readOnly} list={credits}
+                    field="credits" legend='Créditos'
+                    />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}> {this.props.submitLabel}</button>
