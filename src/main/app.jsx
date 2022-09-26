e5e12846bff293267bc9082859aa8bfe0f4082ac
Guilherme <mttsguil@gmail.com>
2022-09-26 08:33:11 -0300

    refec: nav with react-router

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import Header from '../common/template/header';
 import Sidebar from '../common/template/Sidebar';
 import Footer from '../common/template/Footer';
-import Routes from './routes';
+
 import Messages from '../common/msg/messages'
 
 export default props => (
@@ -12,7 +12,7 @@ export default props => (
         <Header />
         <Sidebar />
         <div className='content-wrapper'>
-           <Routes />
+         {props.children}
         </div>
         <Footer />
         <Messages/>
